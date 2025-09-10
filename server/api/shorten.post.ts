01027c619e96f9ed4be78a4f3437b49057a1432a
sakitibi <sakitibi10nin@icloud.com>
2025-09-10 22:54:19 +0900

    コミットされました

--- server/api/shorten.post.ts
+++ server/api/shorten.post.ts
@@ -23,5 +23,5 @@ export default defineEventHandler(async (event) => {
         throw createError({ statusCode: 500, statusMessage: "DB insert failed" });
     }
 
-    return { shortUrl: `${getRequestURL(event).origin}/${id}` };
+    return { shortUrl: `${getRequestURL(event).origin}/${id}`, outputDesc: body.description };
 });
