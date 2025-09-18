37997efc69835f9b92835dd3b4a338aab6d152ec
sakitibi <sakitibi10nin@icloud.com>
2025-09-18 18:03:19 +0900

    コミットされました

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 export const supabase:SupabaseClient = createClient(
-    process.env.NUXT_PUBLIC_SUPABASE_URL!,
-    process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY! // サーバー専用のキー
+    process.env.NUXT_SUPABASE_URL!,
+    process.env.NUXT_SUPABASE_ANON_KEY! // サーバー専用のキー
 );
