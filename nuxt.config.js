7f411922e58447ece34f48f7a8a9418147e12d98
sakitibi <sakitibi10nin@icloud.com>
2025-12-25 12:37:14 +0900

    コミットされました

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
     runtimeConfig: {
         public: {
             supabaseUrl: process.env.NUXT_SUPABASE_URL,
-            supabaseAnonKey: process.env.NUXT_SUPABASE_ANON_KEY,
+            supabaseServiceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
         },
     },
 });
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,6 +4,6 @@ export const useSupabase = () => {
     const config = useRuntimeConfig();
     return createClient(
         config.public.supabaseUrl as string,
-        config.public.supabaseAnonKey as string
+        config.public.supabaseServiceRoleKey as string
     );
 };
