0b85cb9a944345e38fa7a08a3c138b59066e1935
sakitibi <sakitibi10nin@icloud.com>
2025-12-25 14:17:12 +0900

    コミットされました

--- server/api/shorten.post.ts
+++ server/api/shorten.post.ts
@@ -3,26 +3,30 @@ import { nanoid } from "nanoid";
 import { useSupabaseServer } from "@/utils/supabase.server";
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody<Partial<{ url: string; description: string }>>(event);
+    try{
+        const body = await readBody<Partial<{ url: string; description: string }>>(event);
 
-    if (!body?.url) {
-        throw createError({ statusCode: 400, statusMessage: "URL is required" });
+        if (!body?.url) {
+            throw createError({ statusCode: 400, statusMessage: "URL is required" });
+        }
+
+        const id = nanoid(Math.floor(Math.random() * 100) + 100);
+        const supabase = useSupabaseServer();
+
+        const { error } = await supabase
+            .from('"13ninad_click_urls"')
+            .insert({ id, url: body.url, description: body.description });
+
+        if (error) {
+            // Supabase のエラー情報をそのままクライアントに返す
+            return { error: true, statusCode: 500, message: error.message, details: error };
+        }
+
+        return {
+            shortUrl: `${getRequestURL(event).origin}/ck/${id}`,
+            outputDesc: body.description,
+        };
+    } catch(e){
+        return { error: true, statusCode: 500, message: e };
     }
-
-    const id = nanoid(Math.floor(Math.random() * 100) + 100);
-    const supabase = useSupabaseServer();
-
-    const { error } = await supabase
-        .from('"13ninad_click_urls"')
-        .insert({ id, url: body.url, description: body.description });
-
-    if (error) {
-        // Supabase のエラー情報をそのままクライアントに返す
-        return { error: true, statusCode: 500, message: error.message, details: error };
-    }
-
-    return {
-        shortUrl: `${getRequestURL(event).origin}/ck/${id}`,
-        outputDesc: body.description,
-    };
 });
