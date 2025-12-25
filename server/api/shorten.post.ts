75a5cf94348a3a7846d2f07868d4f2261dacbb80
sakitibi <sakitibi10nin@icloud.com>
2025-12-25 11:58:45 +0900

    コミットされました

--- server/api/shorten.post.ts
+++ server/api/shorten.post.ts
@@ -1,27 +1,39 @@
-import { defineEventHandler, readBody, HTTPError, getRequestURL } from "h3";
+import { defineEventHandler, readBody, createError, getRequestURL } from "h3";
 import { nanoid } from "nanoid";
 import { useSupabase } from "@/utils/supabase";
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody<{ url: string, description: string | null}>(event);
+    const body = await readBody<{ url: string; description: string | null }>(event);
+
     if (!body?.url) {
-        throw new HTTPError({ statusCode: 400, statusMessage: "URL is required" });
+        throw createError({
+            statusCode: 400,
+            statusMessage: "URL is required",
+        });
     }
-    const idlength:number = Math.floor(Math.random() * 100) + 100
-    const id = nanoid(idlength);
+
+    const idLength = Math.floor(Math.random() * 100) + 100;
+    const id = nanoid(idLength);
+
     const supabase = useSupabase();
     const { error } = await supabase
         .from("13ninad.click_urls")
         .insert({
             id,
             url: body.url,
-            description: body.description
+            description: body.description,
         });
 
     if (error) {
         console.error(error);
-        throw new HTTPError({ statusCode: 500, statusMessage: "DB insert failed" });
+        throw createError({
+            statusCode: 500,
+            statusMessage: "DB insert failed",
+        });
     }
 
-    return { shortUrl: `${getRequestURL(event).origin}/ck/${id}`, outputDesc: body.description };
+    return {
+        shortUrl: `${getRequestURL(event).origin}/ck/${id}`,
+        outputDesc: body.description,
+    };
 });
