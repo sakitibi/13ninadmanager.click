8b2bef0b88470f947e21d455fe822dcf189c34e0
sakitibi <sakitibi10nin@icloud.com>
2026-05-30 10:01:48 +0900

    Merge remote changes

--- server/api/videoid.post.ts
+++ server/api/videoid.post.ts
@@ -1,9 +1,13 @@
 export default defineEventHandler(async (event: any) => {
     const body = await readBody(event)
 
-    const response = await fetch(
-        `https://asakura-wiki.vercel.app/api/youtube/playlists?list=${body}`,
-    );
+    const response = await fetch("https://asakura-wiki.vercel.app/api/youtube/playlists", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({urls: [body]})
+    })
     const data = await response.json();
     let videoIdArray = [];
     for (let i = 0;i < data.length;i++) {
