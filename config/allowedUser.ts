1bdd071251753fc74cc2e8f9d7a485b4327bb760
sakitibi <sakitibi10nin@icloud.com>
2026-05-30 14:06:09 +0900

    Merge remote changes

--- config/allowedUser.ts
+++ config/allowedUser.ts
@@ -1,4 +1,9 @@
 export const ALLOWED_USER_IDS:string[] = [ // 管理者のuser_idリスト
     "9d2347a2-6322-4d55-93f1-71d8440e5f32",
-    "f2ee27d8-5a46-4539-be74-b02eb6a789c3"
+    "1ee5f173-c538-4bf3-91a0-1899f6c28077",
+    "ba159f68-814f-4564-935f-cea46624fd53",
+    "96b00cd8-d43b-4a5b-b473-84433c0b3c98",
+    "76ee0ae2-ac40-445f-b728-8fa588c3d1a5",
+    "a82a5ff2-6918-4205-8b00-a443df1e65da",
+    "b31cd12c-5312-4a4d-aed6-0f0f98babb7e"
 ];
